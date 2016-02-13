c48a9e21ed5573c74686bf60cba8959ce5df0a2b
DonovanDeSmedt <donovandesmedt@live.be>
2016-02-13 12:51:24 +0100

    Zoekfunctie verder afwerken

--- DidactischeLeermiddelen/Controllers/CatalogusController.cs
+++ DidactischeLeermiddelen/Controllers/CatalogusController.cs
@@ -21,11 +21,65 @@ namespace DidactischeLeermiddelen.Controllers
             this.doelgroepRepository = doelgroepRepository;
             this.leergebiedRepository = leergebiedRepository;
         }
-        public ActionResult Index(int doelgroepId = 0, int leergebiedId = 0)
+        public ActionResult Index()
+        {
+            List<Materiaal> materiaal = materiaalRepository.FindAll().ToList();
+            
+            MaterialenViewModel vm = new MaterialenViewModel()
+            {
+                Materialen = materiaal.Select(b => new MateriaalViewModel(b)),
+                Doelgroepen = doelgroepRepository.FindAll().ToList(),
+                Leergebieden = leergebiedRepository.FindAll().ToList(),
+            };
+            ViewBag.Doelgroepen = GetDoelgroepenSelectedList();
+            ViewBag.Leergebieden = GetLeergebiedSelectedList();
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("Catalogus1", vm);
+            }
+            return View(vm);
+        }
+        [HttpPost]
+        public ActionResult Filter(int[] doelgroepenLijst, int[] leergebiedenLijst)
+        {
+            List<Materiaal> materialen = new List<Materiaal>();
+            //Indien er geen checkboxen aangeklikt werden zulllen alle materialen getoont worden.
+            if (doelgroepenLijst == null && leergebiedenLijst == null)
+            {
+                materialen = materiaalRepository.FindAll().ToList();
+            }
+            //Per leergebied en doelgroep worden alle materialen van de desbetreffende doelgroepen en
+            // leergebieden in de lijst gestoken en doorgegeven naar het vm
+            else
+            {
+                leergebiedenLijst.ForEach(i =>
+                {
+                    materialen.AddRange(materiaalRepository.FindByLeergebied(i));
+                });
+                doelgroepenLijst.ForEach(i =>
+                {
+                    materialen.AddRange(materiaalRepository.FindByDoelgroep(i));
+                });
+            }
+            
+            MaterialenViewModel vm = new MaterialenViewModel()
+            {
+                Materialen = materialen.Distinct().Select(b => new MateriaalViewModel(b)),
+                Doelgroepen = doelgroepRepository.FindAll().ToList(),
+                Leergebieden = leergebiedRepository.FindAll().ToList(),
+            };
+            ViewBag.Doelgroepen = GetDoelgroepenSelectedList();
+            ViewBag.Leergebieden = GetLeergebiedSelectedList();
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("Catalogus1", vm);
+            }
+            return View("Catalogus1", vm);
+        }
+
+        public ActionResult FilterMobile(int doelgroepId = 0, int leergebiedId = 0)
         {
             List<Materiaal> materiaal;
-            Leergebied leergebied;
-            Doelgroep doelgroep;
             if (doelgroepId == 0 && leergebiedId == 0)
             {
                 materiaal = materiaalRepository.FindAll().ToList();
@@ -56,9 +110,8 @@ namespace DidactischeLeermiddelen.Controllers
             {
                 return PartialView("Catalogus1", vm);
             }
-            return View(vm);
+            return View("Catalogus1", vm);
         }
-      
 
         public ActionResult VoegAanVerlanglijstToe(int id, int aantal, Verlanglijst verlanglijst)
         {
@@ -77,7 +130,7 @@ namespace DidactischeLeermiddelen.Controllers
             }
             return RedirectToAction("Index");
         }
-        public ActionResult Zoek(String trefwoord)
+        public ActionResult Zoek(string trefwoord)
         {
 
             //LijstMaken waar we het gezochte materiaal vinden
--- DidactischeLeermiddelen/Scripts/catalogusScript.js
+++ DidactischeLeermiddelen/Scripts/catalogusScript.js
@@ -1,13 +1,15 @@
 ﻿var ViewModel = {
     trefwoord: null,
+    doelgroepen: [],
+    leergebieden: [],
     init : function() {
         $("#doelgroepLijst").change(function () {
-            $.get("Catalogus/Index", { doelgroepId: $("#doelgroepLijst").val(), leergebiedId: $("#leergebiedLijst").val() }, function (data) {
+            $.get("Catalogus/FilterMobile", { doelgroepId: $("#doelgroepLijst").val(), leergebiedId: $("#leergebiedLijst").val() }, function (data) {
                 $("#catalogus").html(data);
             });
         });
         $("#leergebiedLijst").change(function () {
-            $.get("Catalogus/Index", { doelgroepId: $("#doelgroepLijst").val(), leergebiedId: $("#leergebiedLijst").val() }, function (data) {
+            $.get("Catalogus/FilterMobile", { doelgroepId: $("#doelgroepLijst").val(), leergebiedId: $("#leergebiedLijst").val() }, function (data) {
                 $("#catalogus").html(data);
             });
         });
@@ -19,23 +21,48 @@
                         $("#catalogus").html(data);
                 });
             }
-
         });
-        $(".checkbox").change(function() {
-
-            var input = $(this).children()[0];
-            var filter = input.id;
+        $("#zoekMobile").click(function () {
+            
+            $.get("Catalogus/Zoek", { trefwoord: $("#inhoudMobile").val() }, function(data) {
+                $("#catalogus").html(data)
+            });
+        });
+        $(".checkbox").change(function () {
+            ViewModel.doelgroepen = [];
+            ViewModel.leergebieden = [];
+            $('input:checkbox:checked').map(function() {
+                var filter = $(this)[0].id;
+                var id = parseInt(filter.substr(filter.length - 1));
                 if (filter.indexOf("doelgroep") > -1) {
-                    filter = parseInt(filter.substr(filter.length - 1));
-                    $.get("Catalogus/Index", { doelgroepId: filter, leergebiedId: 0 }, function (data) {
-                        $("#catalogus").html(data);
-                    });
-                } else {
-                    filter = parseInt(filter.substr(filter.length - 1));
-                    $.get("Catalogus/Index", { doelgroepId: 0, leergebiedId: filter }, function (data) {
-                        $("#catalogus").html(data);
-                    });
+                        ViewModel.doelgroepen.push(id);
                 }
+                if (filter.indexOf("leergebied") > -1) {
+                        ViewModel.leergebieden.push(id);
+                }
+            });
+            $.ajax({
+                type: "POST",
+                traditional: true, 
+                url: "Catalogus/Filter",
+                data: { doelgroepenLijst: ViewModel.doelgroepen, leergebiedenLijst: ViewModel.leergebieden },
+                success: function (data) {
+                    $("#catalogus").html(data);
+                }
+            });
+            //var input = $(this).children()[0];
+            //var filter = input.id;
+            //    if (filter.indexOf("doelgroep") > -1) {
+            //        filter = parseInt(filter.substr(filter.length - 1));
+            //        $.get("Catalogus/Index", { doelgroepId: filter, leergebiedId: 0 }, function (data) {
+            //            $("#catalogus").html(data);
+            //        });
+            //    } else {
+            //        filter = parseInt(filter.substr(filter.length - 1));
+            //        $.get("Catalogus/Index", { doelgroepId: 0, leergebiedId: filter }, function (data) {
+            //            $("#catalogus").html(data);
+            //        });
+            //    }
         });
     }
 }
