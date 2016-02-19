e5459bd83c17ab1f14dd9e74982639a7fee624c1
thomasledoux1 <thomas.ledoux.q1069@student.hogent.be>
2016-02-19 15:01:45 +0100

    [Sprint2 #5] Details aan de reservatie geven en tonen in de view (kan opgevraagd worden)

---
+++ DidactischeLeermiddelen/ViewModels/ReservatieViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DidactischeLeermiddelen.Models.Domain;
+
+namespace DidactischeLeermiddelen.ViewModels
+{
+    public class ReservatieViewModel : IViewModel
+    {
+        public int MateriaalId { get; set; }
+        public string Foto { get; set; }
+        public string Naam { get; set; }
+        public string Omschrijving { get; set; }
+        public Status Status { get; set; }
+        public DateTime StartDatum { get; set; }
+        public DateTime EindDatum { get; set; }
+
+        public ReservatieViewModel(Materiaal materiaal)
+        {
+            MateriaalId = materiaal.MateriaalId;
+            Foto = materiaal.Foto;
+            Naam = materiaal.Naam;
+            Omschrijving = materiaal.Omschrijving;
+            Status = materiaal.Status;
+            materiaal.ReservatieData.OrderBy(b => b.Day);
+            StartDatum = materiaal.ReservatieData.FirstOrDefault();
+            EindDatum = StartDatum.AddDays(4);
+        }
+    }
+
+    public class ReservatieMaterialenViewModel : IViewModel
+    {
+        public IEnumerable<ReservatieViewModel> Materialen { get; set; }
+
+    }
+}
