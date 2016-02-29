62033e7a2c243545f5a9c76159ed003468ce5ff2
thomasledoux1 <thomas.ledoux.q1069@student.hogent.be>
2016-02-29 18:33:00 +0100

    [Sprint2 #26] implementeren feedback technische demo : Abstract Factory gebruiken voor aanmaken ViewModels

---
+++ DidactischeLeermiddelen/Models/Domain/MaterialenViewModelFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DidactischeLeermiddelen.ViewModels;
+
+namespace DidactischeLeermiddelen.Models.Domain
+{
+    public class MaterialenViewModelFactory : ViewModelFactory
+    {
+        public override IViewModel CreateViewModel(SelectList doelgroepen, SelectList leergebieden, IEnumerable<Materiaal> lijst = null,
+            DateTime startDatum = new DateTime(), Gebruiker gebruiker = null)
+        {
+            IViewModel vm = new MaterialenViewModel()
+            {
+                Materialen = lijst.Select(b => new MateriaalViewModel(b)),
+                DoelgroepSelectList = new DoelgroepViewModel(doelgroepen),
+                LeergebiedSelectList = new LeergebiedViewModel(leergebieden)
+            };
+            return vm;
+        }
+    }
+}
---
+++ DidactischeLeermiddelen/Models/Domain/VerlanglijstMaterialenViewModelFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DidactischeLeermiddelen.ViewModels;
+
+namespace DidactischeLeermiddelen.Models.Domain
+{
+    public class VerlanglijstMaterialenViewModelFactory : ViewModelFactory
+    {
+        public override  IViewModel CreateViewModel(SelectList doelgroepen, SelectList leergebieden, IEnumerable<Materiaal> lijst = null,
+            DateTime startDatum = new DateTime(), Gebruiker gebruiker = null)
+        {
+            IViewModel vmm = new VerlanglijstMaterialenViewModel()
+            {
+                Materialen = gebruiker.Verlanglijst.Materialen.Select(b => new VerlanglijstViewModel(b, startDatum))
+            };
+            return vmm;
+        }
+    }
+}
