16844da0140e92ed99d9729c07c129d6fd560b4d
DonovanDeSmedt <donovandesmedt@live.be>
2016-03-09 21:02:50 +0100

    Refactor js + small fixes

---
+++ DidactischeLeermiddelen/Models/Domain/DtoObjects/ReservatieDataDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DidactischeLeermiddelen.Models.Domain.DtoObjects
+{
+    public class ReservatieDataDTO
+    {
+        public int Aantal { get; set; }
+        public DateTime StartDatum { get; set; }
+    }
+}
