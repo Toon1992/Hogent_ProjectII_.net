adf49568a25ea1cbd5765fe42cb9e61eb7b98302
DonovanDeSmedt <donovandesmedt@live.be>
2016-03-16 20:13:27 +0100

    Testing verlanglijstcontroller

--- DidactischeLeermiddelen.Tests/Controllers/VerlanglijstControllerTest.cs
+++ DidactischeLeermiddelen.Tests/Controllers/VerlanglijstControllerTest.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Web.Mvc;
 using DidactischeLeermiddelen.Controllers;
 using DidactischeLeermiddelen.Models.Domain;
 using DidactischeLeermiddelen.Tests.Domain;
+using DidactischeLeermiddelen.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -15,43 +19,213 @@ namespace DidactischeLeermiddelen.Tests.Controllers
         private ReservatieController reservatieController;
         private Mock<IMateriaalRepository> mockMateriaalRepository;
         private Mock<IGebruikerRepository> mockGebruikerRepository;
-        private Gebruiker gebruiker;
+        private Gebruiker student;
+        private Gebruiker lector;
         private Materiaal m;
         private DummyContext context;
+        private string[] dagenString;
+        private IList<DateTime> dagen;
+        private Reservatie reservatieLector;
+        private Reservatie reservatieStudent;
+        private int[] materiaalIds;
         [TestInitialize]
         public void OpzettenContext()
         {
             context = new DummyContext();
-            gebruiker = context.Toon;
+            student = context.Toon;
+            lector = context.LectorGebruiker;
             mockMateriaalRepository = new Mock<IMateriaalRepository>();
             mockGebruikerRepository = new Mock<IGebruikerRepository>();
             mockMateriaalRepository.Setup(t => t.FindAll()).Returns(context.Materialen);
             mockMateriaalRepository.Setup(t => t.FindById(1)).Returns(context.Encyclopedie);
+            mockMateriaalRepository.Setup(t => t.FindById(2)).Returns(context.GeoDriehoek);
+            mockMateriaalRepository.Setup(t => t.FindById(3)).Returns(context.Bol);
+            mockMateriaalRepository.Setup(t => t.FindById(4)).Returns(context.Kaart);
             mockGebruikerRepository.Setup(t => t.FindByName("student@student.hogent.be")).Returns(context.Toon);
-
             m = context.Bol;
-
+            materiaalIds = new []{ m.MateriaalId };
             verlanglijstController = new VerlanglijstController(mockMateriaalRepository.Object, mockGebruikerRepository.Object);
         }
         [TestMethod]
-        public void ControleInvoer8Beschikbaar5GeselecteerdReturnTrue()
+        public void ControleInvoerStudent8Beschikbaar5GeselecteerdReturnTrue()
         {
             Assert.AreEqual(10, m.AantalInCatalogus);
-            m.AddReservatie(context.ReservatieWeek1Aantal2);
-            int[] materiaalIds = {m.MateriaalId};
+            m.AddReservatie(context.ReservatieWeek1Aantal2Student);
             int[] aantal = {5};
-            verlanglijstController.Controle(gebruiker, materiaalIds, aantal, false, context.StartDatum, null);
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(student, materiaalIds, aantal, startDatum, eindDatum, null);
+            Assert.IsTrue(beschikbaar);
+            Assert.AreEqual(8, m.GeefAantalBeschikbaar(startDatum, eindDatum, null, student));
         }
+        [TestMethod]
+        public void ControleInvoerStudent0Beschikbaar5GeselecteerdReturnFalse()
+        {
+            m.AddReservatie(context.ReservatieWeek1Aantal2Student);
+            m.AddReservatie(context.ReservatieWeek1Aantal8Student);
+            int[] aantal = { 5 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(student, materiaalIds, aantal, startDatum, eindDatum, null);
+            Assert.IsFalse(beschikbaar);
+            Assert.AreEqual(0, m.GeefAantalBeschikbaar(startDatum, eindDatum, null, student));
+        }
+        [TestMethod]
+        public void ControleInvoerStudent5Beschikbaar8GeselecteerdReturnTrue()
+        {
+            m.AddReservatie(context.ReservatieWeek1Aantal5Student);
+            int[] aantal = { 8 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(student, materiaalIds, aantal, startDatum, eindDatum, null);
+            Assert.IsFalse(beschikbaar);
+            Assert.AreEqual(5, m.GeefAantalBeschikbaar(startDatum, eindDatum, null, student));
+        }
+        [TestMethod]
+        public void ControleInvoerStudent5Beschikbaar2GeselecteerdReturnTrue()
+        {
+            m.AddReservatie(context.ReservatieWeek1Aantal5Student);
+            int[] aantal = { 2 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(student, materiaalIds, aantal, startDatum, eindDatum, null);
+            Assert.IsTrue(beschikbaar);
+            Assert.AreEqual(5, m.GeefAantalBeschikbaar(startDatum, eindDatum, null, student));
+        }
+        [TestMethod]
+        public void ControleInvoerLector2Gereserveerd5GeselecteerdReturnTrue()
+        {
+            Assert.AreEqual(10, m.AantalInCatalogus);
+            m.AddReservatie(context.ReservatieWeek1Aantal2Student);
+            int[] aantal = { 5 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(lector, materiaalIds, aantal, startDatum, eindDatum, null);
+            Assert.IsTrue(beschikbaar);
+            Assert.AreEqual(10, m.GeefAantalBeschikbaar(startDatum, eindDatum, null, lector));
+        }
+        [TestMethod]
+        public void ControleInvoerLector10Gereserveerd5GeselecteerdReturnTrue()
+        {
+            m.AddReservatie(context.ReservatieWeek1Aantal2Student);
+            m.AddReservatie(context.ReservatieWeek1Aantal8Student);
+            int[] aantal = { 5 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(lector, materiaalIds, aantal, startDatum, eindDatum, null);
+            Assert.IsTrue(beschikbaar);
+            Assert.AreEqual(10, m.GeefAantalBeschikbaar(startDatum, eindDatum, null, lector));
+        }
+        [TestMethod]
+        public void ControleInvoerLector5Beschikbaar8GeselecteerdReturnTrue()
+        {
+            m.AddReservatie(context.ReservatieWeek1Aantal5Student);
+            int[] aantal = { 8 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(lector, materiaalIds, aantal, startDatum, eindDatum, null);
+            Assert.IsTrue(beschikbaar);
+            Assert.AreEqual(10, m.GeefAantalBeschikbaar(startDatum, eindDatum, null, lector));
+        }
+        [TestMethod]
+        public void ControleInvoerLector5Geblokkeerd2GeselecteerdZelfdeDataReturnTrue()
+        {
+            dagenString = new[] { "15/03/2016"};
+            MaakBlokkeringLector(dagenString, 5);
 
-        //[TestMethod]
-        //public void ReservatieDetailsGrafiekGeeftJsonTerug()
-        //{
-        //    var result = controller.ReservatieDetailsGrafiek(2, 1) as JsonResult;
-        //    var data = result.Data;
-        //    var type = data.GetType();
-        //    var countPropertyInfo = type.GetProperty("Count");
-        //    var expectedCount = countPropertyInfo.GetValue(data, null);
-        //    Assert.AreEqual(4, expectedCount);
-        //}
+            int[] aantal = { 2 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(lector, materiaalIds, aantal, startDatum, eindDatum, dagen);
+            Assert.IsTrue(beschikbaar);
+            Assert.AreEqual(5, m.GeefAantalBeschikbaar(startDatum, eindDatum, dagen,lector));
+        }
+        [TestMethod]
+        public void ControleInvoerLector8Geblokkeerd2GeselecteerdZelfdeDataReturnTrue()
+        {
+            dagenString = new[] { "15/03/2016", "16/03/2016", "17/03/2016" };
+            MaakBlokkeringLector(dagenString, 8);
+
+            int[] aantal = { 2 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(lector, materiaalIds, aantal, startDatum, eindDatum, dagen);
+            Assert.IsTrue(beschikbaar);
+            Assert.AreEqual(2, m.GeefAantalBeschikbaar(startDatum, eindDatum, dagen, lector));
+        }
+        [TestMethod]
+        public void ControleInvoerLector8Geblokkeerd8GeselecteerdZelfdeDataReturnFalse()
+        {
+            dagenString = new[] { "15/03/2016", "16/03/2016" };
+            MaakBlokkeringLector(dagenString, 8);
+
+            int[] aantal = { 8 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(lector, materiaalIds, aantal, startDatum, eindDatum, dagen);
+            Assert.IsFalse(beschikbaar);
+            Assert.AreEqual(2, m.GeefAantalBeschikbaar(startDatum, eindDatum, dagen, lector));
+        }
+        [TestMethod]
+        public void ControleInvoerLector8Geblokkeerd8GeselecteerdOverschrijvendeDataReturnTrue()
+        {
+            dagenString = new[] { "15/03/2016", "16/03/2016" };
+            MaakBlokkeringLector(dagenString, 8);
+
+            dagenString = new[] { "16/03/2016", "18/03/2016" };
+            dagen = dagenString.Select(d => Convert.ToDateTime(d)).ToList();
+            int[] aantal = { 8 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(lector, materiaalIds, aantal, startDatum, eindDatum, dagen);
+            Assert.IsFalse(beschikbaar);
+            Assert.AreEqual(2, m.GeefAantalBeschikbaar(startDatum, eindDatum, dagen, lector));
+        }
+        [TestMethod]
+        public void ControleInvoerLector8Geblokkeerd8GeselecteerdVerschillendeDataReturnTrue()
+        {
+            dagenString = new[] { "15/03/2016", "16/03/2016" };
+            MaakBlokkeringLector(dagenString, 8);
+
+            dagenString = new[] { "17/03/2016", "18/03/2016", "21/03/2016" };
+            dagen = dagenString.Select(d => Convert.ToDateTime(d)).ToList();
+            int[] aantal = { 8 };
+            DateTime startDatum = Convert.ToDateTime(context.StartDatum);
+            DateTime eindDatum = startDatum.AddDays(4);
+            bool beschikbaar = verlanglijstController.ControleSelecteerdMateriaal(lector, materiaalIds, aantal, startDatum, eindDatum, dagen);
+            Assert.IsTrue(beschikbaar);
+            Assert.AreEqual(10, m.GeefAantalBeschikbaar(startDatum, eindDatum, dagen, lector));
+        }
+        [TestMethod]
+        public void ReservatieDetails4Geblokeerd5Gereserveerd()
+        {
+            m.AddReservatie(context.ReservatieWeek1Aantal5Student);
+            dagenString = new[] { "15/03/2016", "16/03/2016" };
+            MaakBlokkeringLector(dagenString, 4);
+            int week = HulpMethode.GetIso8601WeekOfYear(Convert.ToDateTime(context.StartDatum));
+            PartialViewResult result = verlanglijstController.ReservatieDetails(student, m.MateriaalId, week) as PartialViewResult;
+            ReservatiesDetailViewModel vm = result.Model as ReservatiesDetailViewModel;
+            Materiaal materiaal = vm.Material;
+            IList<Reservatie> reservaties = materiaal.Reservaties;
+            Assert.AreEqual(2, materiaal.Reservaties.Count);
+            Assert.AreEqual(m.Reservaties, materiaal.Reservaties);
+        }
+        [TestMethod]
+        public void VerwijderMateriaalUitVerlanglijst()
+        {
+            student.VoegMateriaalAanVerlanglijstToe(m);
+            Assert.AreEqual(1, student.Verlanglijst.Materialen.Count);
+            Assert.AreEqual(m, student.Verlanglijst.Materialen.FirstOrDefault());
+            RedirectToRouteResult result = verlanglijstController.VerwijderUitVerlanglijst(m.MateriaalId, student) as RedirectToRouteResult;
+            Assert.AreEqual(0, student.Verlanglijst.Materialen.Count);
+            mockGebruikerRepository.Verify(m => m.SaveChanges(), Times.Once);
+            Assert.AreEqual("Index", result.RouteValues["Action"]);
+        }
+        private void MaakBlokkeringLector(string[] dagenString, int aantal)
+        {
+            dagen = dagenString.Select(d => Convert.ToDateTime(d)).ToList();
+            reservatieLector = new BlokkeringLector(context.LectorGebruiker, m, context.StartDatum, aantal, dagenString);
+            m.AddReservatie(reservatieLector);
+        }
     }
 }
--- DidactischeLeermiddelen.Tests/Domain/DummyContext.cs
+++ DidactischeLeermiddelen.Tests/Domain/DummyContext.cs
@@ -23,10 +23,17 @@ namespace DidactischeLeermiddelen.Tests.Domain
 
         public Materiaal Kaart { get; set; }
         public Materiaal GeoDriehoek { get; set; }
-        public Reservatie ReservatieWeek1Aantal2 { get; set; }
-        public Reservatie ReservatieWeek1Aantal8 { get; set; }
-        public Reservatie ReservatieWeek1Aantal5 { get; set; }
-        public Reservatie ReservatieWeek2Aantal2 { get; set; }
+        public Reservatie ReservatieWeek1Aantal2Student { get; set; }
+        public Reservatie ReservatieWeek1Aantal8Student { get; set; }
+        public Reservatie ReservatieWeek1Aantal5Student { get; set; }
+        public Reservatie ReservatieWeek1Aantal2Lector { get; set; }
+        public Reservatie ReservatieWeek1Aantal8Lector { get; set; }
+        public Reservatie ReservatieWeek1Aantal5Lector { get; set; }
+        public Reservatie ReservatieWeek1Aantal10Lector { get; set; }
+        public string[] Dagen1 { get; set; }
+        public string[] Dagen2 { get; set; }
+        public string[] Dagen3 { get; set; }
+        public string[] Dagen4 { get; set; }
         public string StartDatum { get; set; }
         public Student Student { get; set; }
 
@@ -80,11 +87,13 @@ namespace DidactischeLeermiddelen.Tests.Domain
 
             Bol = new Materiaal("Wereldbol", 456, 10)
             {
-                IsReserveerBaar = true
+                IsReserveerBaar = true,
+                MateriaalId = 3
             };
             Kaart = new Materiaal("Kaart", 4587, 5)
             {
-                IsReserveerBaar = true
+                IsReserveerBaar = true,
+                MateriaalId = 4
             }; 
             Encyclopedie = new Materiaal("Encyclopedie", 111, 80)
             {
@@ -99,10 +108,17 @@ namespace DidactischeLeermiddelen.Tests.Domain
 
             };
             StartDatum = "14/03/2016";
-            ReservatieWeek1Aantal2 = new ReservatieStudent(Toon, Bol, StartDatum, 2);
-            ReservatieWeek1Aantal8 = new ReservatieStudent(Toon, Bol, StartDatum, 8);
-            ReservatieWeek1Aantal5 = new ReservatieStudent(Toon, Bol, StartDatum, 5);
-            ReservatieWeek2Aantal2 = new ReservatieStudent(Toon, GeoDriehoek, StartDatum, 2);
+            Dagen1 = new []{"15/03/2016", "16/03/2016"};
+            Dagen2 = new[] { "16/03/2016", "17/03/2016" };
+            Dagen3 = new[] { "15/03/2016", "18/03/2016" };
+            Dagen4 = new[] { "21/03/2016", "22/03/2016" };
+            ReservatieWeek1Aantal2Student = new ReservatieStudent(Toon, Bol, StartDatum, 2);
+            ReservatieWeek1Aantal8Student = new ReservatieStudent(Toon, Bol, StartDatum, 8);
+            ReservatieWeek1Aantal5Student = new ReservatieStudent(Toon, Bol, StartDatum, 5);
+            ReservatieWeek1Aantal2Lector = new BlokkeringLector(LectorGebruiker, Bol, StartDatum, 2,Dagen1);
+            ReservatieWeek1Aantal8Lector = new BlokkeringLector(LectorGebruiker, Bol, StartDatum, 8, Dagen2);
+            ReservatieWeek1Aantal5Lector = new BlokkeringLector(LectorGebruiker, Bol, StartDatum, 5, Dagen3);
+            ReservatieWeek1Aantal10Lector = new BlokkeringLector(LectorGebruiker, Bol, StartDatum, 10, Dagen4);
             //Reservatie = new Reservatie();
             Student = new Student();
             materialen.Add(Bol);
