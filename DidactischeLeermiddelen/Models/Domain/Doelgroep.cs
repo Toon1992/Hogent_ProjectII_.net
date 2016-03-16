d3953ba4cedc3a048ad9a79e397d97eb61f6a6c8
DonovanDeSmedt <donovandesmedt@live.be>
2016-03-16 22:43:03 +0100

    merge

--- DidactischeLeermiddelen/Models/Domain/Doelgroep.cs
+++ DidactischeLeermiddelen/Models/Domain/Doelgroep.cs
@@ -5,7 +5,6 @@
         #region fields
         public int DoelgroepId { get; set; }
         public string Naam { get; set; }
-        public virtual List<Materiaal> Materialen { get; set; }
         #endregion
     }
 }
--- DidactischeLeermiddelen/Models/Domain/Leergebied.cs
+++ DidactischeLeermiddelen/Models/Domain/Leergebied.cs
@@ -5,7 +5,6 @@
         #region fields
         public int LeergebiedId { get; set; }
         public string Naam { get; set; }
-        public virtual List<Materiaal> Materialen { get; set; }
         #endregion
     }
 }
